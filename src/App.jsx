eb7b2d1ac04c75a9a92600c67a59ccfde692f270
BoltonAndrew <boltonandrew@live.co.uk>
2023-08-23 16:03:45 +0100

    basic code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Product from "./components/Product.jsx";
+import style from "./App.module.css";
+
+function App() {
+  return (
+    <div className={style.App}>
+      <Product />
+    </div>
+  );
+}
+
+export default App;
