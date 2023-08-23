27b5bd4a054ec1d2ee398fcdff6bfe174d374585
BoltonAndrew <boltonandrew@live.co.uk>
2023-08-23 18:56:02 +0100

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,14 @@ import style from "./App.module.css";
 function App() {
   return (
     <div className={style.App}>
+      <header>
+        <h1>State Management</h1>
+        <h2>useReducer and/or useContext</h2>
+      </header>
       <Product />
+      <footer>
+        <h3>Created By Andy Bolton</h3>
+      </footer>
     </div>
   );
 }
